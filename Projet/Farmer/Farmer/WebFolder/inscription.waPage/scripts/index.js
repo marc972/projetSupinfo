44a84a67f3ade07c9ed9445d3763c00692b5c1dc
Marc <chery.marc972@hotmail.com>
2013-02-04 12:01:15 +0100

    création formulaire

---
+++ Projet/Farmer/Farmer/WebFolder/inscription.waPage/scripts/index.js
@@ -0,0 +1,41 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var inscription = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	inscription.click = function inscription_click (event)// @startlock
+	{// @endlock
+		var newJoueur = ds.Joueur.newEntity();
+		var email = document.getElementById("EMAIL").value;
+		var motDePasse = document.getElementById("MOT_DE_PASSE").value;
+		var reg = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+		
+		if(reg.test(email)){
+		newJoueur.email.setValue(email);
+		newJoueur.motDePasse.setValue(motDePasse);
+		ds.Joueur.find('email = "' + newJoueur.email.getValue() + '"', {
+			onSuccess:function(event) {
+				if(event.entity == null){
+					newJoueur.save({
+			onSuccess:function(event) {alert("saved");}
+			});
+		}
+		else{
+			alert("Cette adresse mail est déjà utilisée");
+			}
+				}
+			});
+		}
+		else{
+			alert("Veuillez entrer une adresse valide");
+			}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("inscription", "click", inscription.click, "WAF");
+// @endregion
+};// @endlock
---
+++ Projet/Farmer/Farmer/WebFolder/login.waPage/scripts/index.js
@@ -0,0 +1,34 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var login = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	login.click = function login_click (event)// @startlock
+	{// @endlock
+		var newJoueur = ds.Joueur.newEntity();
+		var email = document.getElementById("EMAIL").value;
+		var motDePasse = document.getElementById("MOT_DE_PASSE").value;
+		var reg = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+		
+		if(reg.test(email)){
+		newJoueur.email.setValue(email);
+		newJoueur.motDePasse.setValue(motDePasse);
+		ds.Joueur.find('email = "' + newJoueur.email.getValue() + '" and motDePasse = "' + newJoueur.motDePasse.getValue() + '"', {
+			onSuccess:function(event) {
+				window.location.href="jeu.waPage/index.html"
+			},
+			onError:function(event) {alert("mot de passe ou email incorrect");}});
+		}
+		else{
+			alert("Veuillez entrer une adresse valide");
+			}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("login", "click", login.click, "WAF");
+// @endregion
+};// @endlock
